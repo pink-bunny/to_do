4e337362861cd15e14f32c6e66cb1f94e2d0b255
Lana Razheva <rajevasvetlana@gmail.com>
2018-05-19 20:03:44 +0300

    Remove message reducer.

--- src/redux-store/reducer.js
+++ src/redux-store/reducer.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as formReducer } from 'redux-form';
 
-import message from './message/reducer';
 import task from './task/reducer';
 
 export default combineReducers({
-  message,
   task,
   form: formReducer,
   router: routerReducer
